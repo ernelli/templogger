3a18ffb51e0e13b4d20cf8f44a86a99dc9e4bf8e
Mattias Ernelli <you@example.com>
2016-12-18 00:22:09 +0100

    Fixed so that external data source can be selected, useful when testing locally
    Added graph label so that graph values can be inspected. TODO, replace derived graphs with true values. Also only include plotted graphs

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+
+HANDLEBARS=`which hanlebars`
+if [[ $HANDLEBARS == "" ]]; then
+    HANDLEBARS=node_modules/handlebars/bin/handlebars
+fi
 
 mkdir -p public/lib
 cp node_modules/requirejs/require.js public/lib
-handlebars -a -p -f public/partials.js *.hbar
+$HANDLEBARS -a -p -f public/partials.js *.hbar
 cp  node_modules/handlebars/dist/handlebars.runtime.js public/
